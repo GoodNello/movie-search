766b27eb37d2f87f7bea1e1caf8eb0b312abc1cf
GoodNello <grislysquirrel@gmail.com>
2018-04-20 16:24:38 +0200

    added movie information

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,10 +3,45 @@ import React from "react";
 export default class Movie extends React.Component {
   render() {
     if (this.props.data) {
-      const { Title } = this.props.data;
+      const {
+        Title,
+        Year,
+        Runtime,
+        Genre,
+        Director,
+        Actors,
+        Plot,
+        Country,
+        Awards,
+        Poster,
+        Production
+      } = this.props.data;
+
       return (
-        <div>
-          <h3>{Title}</h3>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <h3>
+                {Title} ({Country} - {Year})
+              </h3>
+              <h6>
+                {Genre} - {Runtime}
+              </h6>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <p className="lead">{Plot}</p>
+              With: {Actors}
+              <ul>
+                <li>Production: {Production}</li>
+                <li>Awards: {Awards}</li>
+              </ul>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <img src={Poster} with="150" />
+            </div>
+          </div>
         </div>
       );
     } else {

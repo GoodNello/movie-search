b77b93573dbd25aa15b5c893e161881b112cd89c
GoodNello <grislysquirrel@gmail.com>
2018-04-23 17:25:37 +0200

    added movie not found message

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -14,35 +14,42 @@ export default class Movie extends React.Component {
         Country,
         Awards,
         Poster,
-        Production
+        Production,
+        Error
       } = this.props.data;
 
       return (
         <div className="container">
-          <div className="row">
-            <div className="col-sm-12">
-              <h3>
-                {Title} ({Country} - {Year})
-              </h3>
-              <h6>
-                {Genre} - {Runtime}
-              </h6>
+          {Error ? (
+            <p>Can't find a movie with that title.</p>
+          ) : (
+            <div>
+              <div className="row">
+                <div className="col-sm-12">
+                  <h3>
+                    {Title} ({Country} - {Year})
+                  </h3>
+                  <h6>
+                    {Genre} - {Runtime}
+                  </h6>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12 col-md-6">
+                  <p className="lead">{Plot}</p>
+                  <p>Directed by {Director}</p>
+                  <p>With {Actors}</p>
+                  <ul>
+                    <li>Production: {Production}</li>
+                    <li>Awards: {Awards}</li>
+                  </ul>
+                </div>
+                <div className="col-sm-12 col-md-6" align="center">
+                  <img alt={`${Title} Poster`} src={Poster} with="150" />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-12 col-md-6">
-              <p className="lead">{Plot}</p>
-              <p>Directed by {Director}</p>
-              <p>With {Actors}</p>
-              <ul>
-                <li>Production: {Production}</li>
-                <li>Awards: {Awards}</li>
-              </ul>
-            </div>
-            <div className="col-sm-12 col-md-6" align="center">
-              <img alt={`${Title} Poster`} src={Poster} with="150" />
-            </div>
-          </div>
+          )}
         </div>
       );
     } else {
